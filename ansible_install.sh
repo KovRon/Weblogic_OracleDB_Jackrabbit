239aba7a62f7f1c348be0fa50674cb4a94d1f172
roman <kron87@gmail.com>
2016-05-02 19:31:14 +0600

    Changes in ansible_install.sh script and Ansible hosts file

--- ansible_install.sh
+++ ansible_install.sh
@@ -3,8 +3,9 @@
 ANSIBLE_ENV=/opt/ansible/hacking/env-setup
 ANSIBLE_PATH=/opt/ansible
 VAGRANT_USER_HOME=/home/vagrant
+ANSIBLE_CODE=/home/vagrant/ansible_code
 
-yum -y install gcc gcc-c++ kernel-devel python-devel libxslt-devel libffi-devel openssl-devel git 
+yum -y install sshpass vim gcc gcc-c++ kernel-devel python-devel libxslt-devel libffi-devel openssl-devel git 
 wget https://bootstrap.pypa.io/ez_setup.py -O - | sudo python
 easy_install pip 
 pip install paramiko PyYAML Jinja2 httplib2 six pycrypto
@@ -12,6 +13,10 @@ git clone https://github.com/ansible/ansible.git --recursive $ANSIBLE_PATH
 cd $ANSIBLE_PATH
 git pull --rebase
 git submodule update --init --recursive
+git clone https://github.com/KovRon/Weblogic_OracleDB_Jackrabbit.git $ANSIBLE_CODE
+mkdir /etc/ansible
+cp $ANSIBLE_CODE/hosts /etc/ansible/
+cp $ANSIBLE_CODE/ansible.cfg /etc/ansible/
 echo 'source '$ANSIBLE_ENV' -q' >> $VAGRANT_USER_HOME/.bashrc
 source $ANSIBLE_ENV -q
 chown -R vagrant:vagrant $ANSIBLE_PATH
